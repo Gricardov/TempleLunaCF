5c51b9bf47fc020bc5757263b56debdfd25acde3
gricardov <gricardov@gmail.com>
2021-05-17 18:26:29 -0500

    fix

--- functions/mail/sender.js
+++ functions/mail/sender.js
@@ -29,7 +29,7 @@ exports.sendEmail = (receiver, receiverName, type = 'REQUEST_DONE', extraData) =
                 altText = `Hola ${receiverName}.\nTu trabajo final puede ser encontrado aquí:\n${linkTo}\nTe esperamos en la mejor comunidad literaria del mundo: ${joinLink}\nEquipo Temple Luna.`;
                 htmlText = mailTemplate.toString()
                     .replace(/{{title}}/g, subject)
-                    .replace(/{{bodyText}}/g, `¡Hola, ${receiverName}!<br/>Uno de nuestros artistas ha tomado tu solicitud. ¡No olvides compartirlo, eso ayuda mucho al artista!`)
+                    .replace(/{{bodyText}}/g, `¡Hola, ${receiverName}!<br/>Uno de nuestros artistas ha tomado tu solicitud. No olvides compartirlo, eso ayuda mucho al artista`)
                     .replace(/{{linkto}}/g, linkTo);
             }
             break;
@@ -43,7 +43,7 @@ exports.sendEmail = (receiver, receiverName, type = 'REQUEST_DONE', extraData) =
                 altText = `Hola ${receiverName}.\nTu trabajo en la obra "${title}" ha recibido un comentario. Leelo en tu cuenta o a través de este link:\n${linkTo}\nNo olvides que te queremos.\nEquipo Temple Luna.`;
                 htmlText = mailTemplate.toString()
                     .replace(/{{title}}/g, subject)
-                    .replace(/{{bodyText}}/g, `¡Hola, ${receiverName}!<br/>Tu trabajo en la obra "${title}" ha recibido un comentario ¡Felicitaciones!. Leelo en tu cuenta o desde este botón:`)
+                    .replace(/{{bodyText}}/g, `¡Hola, ${receiverName}!<br/>Tu trabajo en la obra "${title}" ha recibido un comentario ¡Felicitaciones!. Leelo en tu cuenta o desde aquí:`)
                     .replace(/{{linkto}}/g, linkTo);
             }
             break;
@@ -57,7 +57,7 @@ exports.sendEmail = (receiver, receiverName, type = 'REQUEST_DONE', extraData) =
                 altText = `Hola ${receiverName}.\nTu trabajo en la obra "${title}" ha recibido un corazón. Míralo desde tu cuenta aquí:\n${linkTo}\nNo olvides que te queremos.\nEquipo Temple Luna.`;
                 htmlText = mailTemplate.toString()
                     .replace(/{{title}}/g, subject)
-                    .replace(/{{bodyText}}/g, `¡Hola, ${receiverName}!<br/>Tu trabajo en la obra "${title}" ha recibido un corazón ¡Felicitaciones!. Leelo desde tu cuenta aquí:`)
+                    .replace(/{{bodyText}}/g, `¡Hola, ${receiverName}!<br/>Tu trabajo en la obra "${title}" ha recibido un corazón ¡Felicitaciones!. Leelo en tu cuenta desde aquí:`)
                     .replace(/{{linkto}}/g, linkTo);
             }
             break;
